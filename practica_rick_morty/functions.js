14110917ad42bdd5e247c38d5cea083a87fec4e0
Julio Hernández <julio_daniel_hm@hotmail.com>
2026-08-31 12:55:01 -0600

    Update functions.js

--- practica_rick_morty/functions.js
+++ practica_rick_morty/functions.js
@@ -5,6 +5,7 @@ async function obtenerPersonajes() {
   try {
     const response = await fetch("https://rickandmortyapi.com/api/character");
     const data = await response.json();
+    console.log ("Personajes obtenidos:", data.results);
     return data.results;
   } catch (error) {
     console.error("Error al obtener los personajes:", error);
